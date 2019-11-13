1484775557f2cef795180240854e772d659c1cf0
Tomas <tomas.lindqvist85@gmail.com>
2019-11-13 13:04:08 +0100

    Add 3 for 2

--- scripts/SoapProducts.js
+++ scripts/SoapProducts.js
@@ -1,21 +1,26 @@
 store.cartContent = store.cartContent || [];
 store.save();
 
-function Soap(name, quantity, price) {
+function Soap(name, quantity, price, campaign) {
     this.Name = name;
     this.Quantity = quantity;
     this.Price = price;
+    this.Campaign = campaign;
 }
 
 function buy(e) {
 
     let antal = e + "Antal";
     let pris = e + "Price";
+    let kampanj = e + "Kampanj";
 
     let quantity = document.getElementById(antal).value;
     let price = document.getElementById(pris).innerHTML;
+    let campaign = document.getElementById(kampanj).value;
 
-    let soap = new Soap(e, quantity, price);
+
+
+    let soap = new Soap(e, quantity, price, campaign);
 
     store.cartContent.push(soap);
     store.save();
