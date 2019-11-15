bc82d4bc0f1abc32ca7490bf1002944f63547427
Carita <caritasmail@gmail.com>
2019-11-15 09:22:52 +0100

    updating basket

--- App_Data/Models/models.generated.cs
+++ App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "9678ed137f40b9cc")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
 
 namespace Umbraco.Web.PublishedModels
 {
--- scripts/SoapProducts.js
+++ scripts/SoapProducts.js
@@ -10,6 +10,7 @@ function Soap(name, quantity, price, campaign) {
 
 function buy(e) {
 
+    
     let antal = e + "Antal";
     let pris = e + "Price";
     let kampanj = e + "Kampanj";
@@ -17,9 +18,23 @@ function buy(e) {
     let quantity = document.getElementById(antal).value;
     let price = document.getElementById(pris).innerHTML;
     let campaign = document.getElementById(kampanj).value;
+    
+    if (quantity < 1 || quantity == "") { 
+        return;
+    }
+    /*else if (store.cartContent.length > 0) {
+        for (var i in store.cartContent) {
+          if (store.cartContent[i].name === this.name) {
+            cart[i].quantity += parseInt(quantity);
+
+            store.save();
+            return;
+              }
+            }
+          } */
 
     let soap = new Soap(e, quantity, price, campaign);
-
+    
     store.cartContent.push(soap);
     store.save();
 }
